c3c30b7c7ca430bf6bebc5fb9be203e987d3269a
AmrNasser <amr931111@gmail.com>
2024-07-07 10:04:07 +0300

    commit

---
+++ src/Main.kt
@@ -0,0 +1,121 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    /*  // Assignment 1 (WhatsApp)
+
+      val phoneNumber = "+20102573949"
+      val checkOTP = 665_354
+      val userName = "Amr Nasser Saad"
+      val bio = "Hello "
+      val isLogIn = true
+      val userStatus = "Hey"
+      val userMuteStatus = false
+      val userProfilePictureUrl = "url"
+      val isOnline = true
+      val lastSeenTimestamp = 1625151600000
+      val chatWith = "Ahmed"
+      val message = "hi"
+      val messageType = "text"
+      val messageReaction = "Smile"
+      val isMessageRead = true
+      val messageWithAttachment = false
+      val groupName = "X  Group"
+      val groupDescription = "Description"
+      val groupMemberCount = 10
+      val uiTheme = "Dark"
+      val language = "English"
+      val appVersion = "1.2.2"
+
+
+      // Assignment 2
+      val appName = "Game"
+      val developer = "Amr Nasser Saad"
+      val category = "Puzzle"
+      val rating = 5.5
+      val numberOfReviews = 2500
+      val description =
+          "Puzzle Game"
+      val price = "40$"
+      val version = "2.1.1"
+      val updateDate = "June 1, 2024"
+      val installs = 1000
+
+
+      // Assignment 3 (YouTube)
+
+      val subscriberNumber = 1500
+      val hoursWatched = 4200
+      val videoType = "Public"
+      val monthsPassed = 12
+      val hasEnoughSubscribers = subscriberNumber >= 1000
+      val hasEnoughWatchHours = hoursWatched >= 4000
+      val isPublicContent = videoType == "Public"
+      val withinTimeFrame = monthsPassed <= 12
+
+
+      if (hasEnoughSubscribers && hasEnoughWatchHours && isPublicContent && withinTimeFrame) {
+          println("The YouTuber can join the YouTube Partner Program and earn money.")
+      } else {
+          println("The YouTuber cannot join the YouTube Partner Program and earn money.")
+      }*/
+
+
+
+
+  /*  // safe _ nullable  (value or null)
+    val n: String? = null
+    val l = if (n != null) { n.length } else { 0 }
+    println(l)
+
+    //elvis opr when using nullable reference
+    val n2: String? = null
+    val l2 = n2?.length?: "empty"
+    println(l2)
+    */
+
+
+ /*  // Assignment 1
+    for(i in 1 .. 6) {
+        println("IAM GROOT")
+    }
+
+    // Assignment 2
+    for(i in 1 .. 100) {
+        if(i%6 == 0)
+            println(i)
+    }
+
+
+  // Assignment 3
+    val grade = 72
+    when(grade){
+        in 90 .. 100 -> println("A+")
+        in 75 .. 89 -> println("B+")
+        in 50 .. 74 -> println("D+")
+        else -> println("D")
+    }
+
+   // Assignment 4
+    val day = 3
+    when(day) {
+        1 -> println("20 MB")
+        2 -> println("25 MB")
+        3 -> println("25 MB")
+        4 -> println("30 MB")
+        5 -> println("30 MB")
+        else -> println("No Gifts")
+    }*/
+
+
+
+    val array = arrayOf(1, 2, 3, 4, 5)
+    for (element in array) {
+        println(element)
+    }
+
+
+
+
+
+}
